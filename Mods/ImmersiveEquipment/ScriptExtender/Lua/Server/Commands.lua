94045fd683c7d5f18462118cf3317c52bd83d703
dragoni7 <sdgibson18@outlook.com>
2024-02-19 16:40:37 -0800

    Version 1.0

--- Mods/ImmersiveEquipment/ScriptExtender/Lua/Server/Commands.lua
+++ Mods/ImmersiveEquipment/ScriptExtender/Lua/Server/Commands.lua
@@ -3,7 +3,10 @@ local IE_ITEMS = {
     "f3c8dc95-e29e-44bf-8df1-d6b5316e3e81",
     "d79c821e-215a-4431-8bd9-5a970865a7cc",
     "dc8f5d8e-76d3-4109-8e42-0f3e16632794",
-    "31ee023c-d151-40f6-917e-84066772783b"
+    "a63de08c-d80c-42fc-8653-979bb302974d",
+    "24fcb568-feb6-480e-b259-440d8b84b4bd",
+    "b2541b90-c8a0-4d9f-a88b-5a8a779045c7",
+    "19e7aa1b-4118-4714-8eee-638263389982"
 }
 
 local function AddIEItems(cmd)
--- Mods/ImmersiveEquipment/ScriptExtender/Lua/Server/Events.lua
+++ Mods/ImmersiveEquipment/ScriptExtender/Lua/Server/Events.lua
@@ -1,8 +1,15 @@
 
-Ext.Osiris.RegisterListener("EnteredCombat", 2, "after", function (guid, combatid)
+Ext.Osiris.RegisterListener("EnteredCombat", 2, "after", function (object, combatGuid)
 
     -- Remove Cloaker Lord Invis
-    if (guid == "IE_Cloaker_Lord_b81dd088-141f-4dc6-89ec-1376aaf55303") then
-        RemoveStatus(guid,"INVISIBLE", "")
+    if (object == "IE_Cloaker_Lord_b81dd088-141f-4dc6-89ec-1376aaf55303") then
+        RemoveStatus(object,"INVISIBLE", "")
+    end
+end)
+
+Ext.Osiris.RegisterListener("StartAttack", 4, "before", function(defender, attacker, storyActionID, attackOwner) 
+    if (HasPassive(attacker, "IE_Source_Hunger") == 1) then
+        ApplyDamage(defender, Osi.GetActionResourceValuePersonal(attacker, "SorceryPoint", 0) * 2, "Necrotic", attacker)
+        PlaySound(attacker, "Spell_Impact_Damage_Necrotic_ChillTouch_L1to3")
     end
 end)
